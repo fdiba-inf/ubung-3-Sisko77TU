4151d517d4c897d8b10103c44aaa6fe677c67a7f
Sisko77TU <siskovat01@abv.bg>
2020-10-13 16:15:53 +0000

    kpr

--- src/exercise3/NumberCounter.java
+++ src/exercise3/NumberCounter.java
@@ -9,26 +9,4 @@ public class NumberCounter {
 
         int positiveCount = 0;
         int negativeCount = 0;
-        double sum = 0;
-        int n = input.nextInt();
-        while(n != 0)
-        {
-          if (n > 0)
-          {
-           positiveCount++;
-          }
-          else
-          {
-          negativeCount++;
-          }
-        sum = sum + n;
-        n = input.nextInt();
-       }
-        System.out.println("Positive numbers: " + positiveCount);
-        System.out.println("Negative numbers: " + negativeCount);
-        System.out.println("Sum: " + sum);
-        double avg = sum / (positiveCount + negativeCount);
-        System.out.println("Average: " + avg);
-
-}
-}
+      
