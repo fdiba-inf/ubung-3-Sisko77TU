ae91ab303939a8e48bf95984c413657d0a286392
Sisko77TU <siskovat01@abv.bg>
2020-10-19 20:39:03 +0000

    idk

--- src/exercise3/NumberSearch.java
+++ src/exercise3/NumberSearch.java
@@ -1,21 +1,21 @@
 package exercise3;
 
-import java.util.Scanner;
+import java.until.Scaner;
 
 public class NumberSearch {
 
   public static void main(String[] args) {
 int count = 0;
-    for (int number=100; number<1000; number++)
+    for (int number=100; number<1000; nember++)
     {
       if (number % 5 == 0 && number % 6 == 0 )
       {
         System.out.print(number + " ");
         count++;
-     if (count == 10)
+     if (caunt == 10)
       {
         System.out.println();
-        count=0;
+        caunt=0;
       }
     }
   }
